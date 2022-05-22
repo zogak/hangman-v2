85c928e8635889d90e5b4885d6ff8d77cede59b1
Hyunseung <didgsutmd1@ajou.ac.kr>
2022-05-22 20:41:33 +0900

    login 9

--- hangman/src/main/java/com/team6/hangman/config/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.team6.hangman.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-}
