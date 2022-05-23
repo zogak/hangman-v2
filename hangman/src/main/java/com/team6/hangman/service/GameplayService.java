fc6bf91ce1b2832e975be10c043b81e42ed4d674
zogak <zogak7@gmail.com>
2022-05-23 21:55:47 +0900

    fix : gameplay

--- hangman/src/main/java/com/team6/hangman/service/GameplayService.java
+++ hangman/src/main/java/com/team6/hangman/service/GameplayService.java
@@ -1,32 +1,32 @@
-package com.team6.hangman.service;
-
-import java.util.ArrayList;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class GameplayService {
-	
-	public GameplayManager findManagerById(Integer gameroomId) {
-		return null;
-	}
-	
-	public boolean isWordCorrect(String targetWord, String letter) {
-		if (targetWord.equals(letter)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean isLetterExists(String targetWord, String letter) {
-		if (targetWord.indexOf(letter) == -1) {
-			return false;
-		}
-		return true;
-	}
-	
-	public ArrayList<Integer> getAllIndexOfLetter(String targetWord, String letter) {
-		
-		return null;
-	}
-}
+//package com.team6.hangman.service;
+//
+//import java.util.ArrayList;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class GameplayService {
+//	
+//	public GameplayManager findManagerById(Integer gameroomId) {
+//		return null;
+//	}
+//	
+//	public boolean isWordCorrect(String targetWord, String letter) {
+//		if (targetWord.equals(letter)) {
+//			return true;
+//		}
+//		return false;
+//	}
+//	
+//	public boolean isLetterExists(String targetWord, String letter) {
+//		if (targetWord.indexOf(letter) == -1) {
+//			return false;
+//		}
+//		return true;
+//	}
+//	
+//	public ArrayList<Integer> getAllIndexOfLetter(String targetWord, String letter) {
+//		
+//		return null;
+//	}
+//}
