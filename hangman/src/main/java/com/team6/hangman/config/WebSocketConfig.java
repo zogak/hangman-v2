84d684bf57eeeddf8d8654394a5b0a53bd885fa2
Hyunseung <didgsutmd1@ajou.ac.kr>
2022-05-26 23:52:09 +0900

    emoji 1

--- hangman/src/main/java/com/team6/hangman/config/WebSocketConfig.java
+++ hangman/src/main/java/com/team6/hangman/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.team6.hangman.config;
 
+import com.team6.hangman.service.ChatSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -11,14 +12,21 @@ import com.team6.hangman.service.WebSocketHandler;
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer{
-	
+
+
 	@Bean
 	public WebSocketHandler websocketHandler() {
 		return new WebSocketHandler();
 	}
 
+	@Bean
+	public ChatSocketHandler chatSocketHandler(){
+		return new ChatSocketHandler();
+	}
+
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(websocketHandler(), "/gameroom/*").setAllowedOrigins("*");
+		registry.addHandler(chatSocketHandler(), "/chat");
 	}
 }
